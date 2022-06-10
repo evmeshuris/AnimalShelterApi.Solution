26f9bfd52d702b4cb2c6d2106eb2af828a48749b
evmeshuris <101578340+evmeshuris@users.noreply.github.com>
2022-06-10 11:40:12 -0700

    initial commit; project setup

---
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AnimalShelter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+namespace AnimalShelter.Controllers
+{ }
---
+++ AnimalShelter/Models/Animals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AnimalShelter.Models
+{
+	public class Animal
+	{
+		public string AnimalId { get; set; }
+		public string Type { get; set; }
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+	}
+}
+
